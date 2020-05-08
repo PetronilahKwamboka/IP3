a7d8a78d4af2e6fda9d2bae1100819cfa82cf6ab
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-08 19:43:29 +0300

    add code on toggle of design icon and description

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+  $("div#design-image").click(function(){
+    $("div#design-image").toggle();
+    $("div#design").toggle();
+  });
+
+  $("div#design").click(function(){
+    $("#design-image").toggle();
+    $("div#design").toggle();
+  });
+});
+
+
+$(document).ready(function(){
+  $("div#design-image").click(function(){
+    $("div#design-image").toggle();
+    $("div#design").toggle();
+  });
+
+  $("div#design").click(function(){
+    $("#design-image").toggle();
+    $("div#design").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("div#design-image").click(function(){
+    $("div#design-image").toggle();
+    $("div#design").toggle();
+  });
+
+  $("div#design").click(function(){
+    $("#design-image").toggle();
+    $("div#design").toggle();
+  });
+});
+
+$(document).ready(function(){
+  $("div#design-image").click(function(){
+    $("div#design-image").toggle();
+    $("div#design").toggle();
+  });
+
+  $("div#design").click(function(){
+    $("#design-image").toggle();
+    $("div#design").toggle();
+  });
+});
