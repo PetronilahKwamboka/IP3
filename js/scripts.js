58b770c7985597097c9e1fcfe1665a0e1b67da1d
PetronilahKwamboka <pettykwamboka@gmail.com>
2020-05-08 19:56:50 +0300

    add code on development, product of toggle effect between the icons and description

--- js/scripts.js
+++ js/scripts.js
@@ -12,37 +12,25 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
-  $("div#design-image").click(function(){
-    $("div#design-image").toggle();
-    $("div#design").toggle();
+  $("div#development-image").click(function(){
+    $("div#development-image").toggle();
+    $("div#development").toggle();
   });
 
-  $("div#design").click(function(){
-    $("#design-image").toggle();
-    $("div#design").toggle();
-  });
-});
-
-$(document).ready(function(){
-  $("div#design-image").click(function(){
-    $("div#design-image").toggle();
-    $("div#design").toggle();
-  });
-
-  $("div#design").click(function(){
-    $("#design-image").toggle();
-    $("div#design").toggle();
+  $("div#development").click(function(){
+    $("#development-image").toggle();
+    $("div#development").toggle();
   });
 });
 
 $(document).ready(function(){
-  $("div#design-image").click(function(){
-    $("div#design-image").toggle();
-    $("div#design").toggle();
+  $("div#product-image").click(function(){
+    $("div#product-image").toggle();
+    $("div#product").toggle();
   });
 
-  $("div#design").click(function(){
-    $("#design-image").toggle();
-    $("div#design").toggle();
+  $("div#product").click(function(){
+    $("#product-image").toggle();
+    $("div#product").toggle();
   });
 });
